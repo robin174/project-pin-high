93f27bc402da7664c3dec6acf020907da7a49c7c
Robin Green <robin@designingreen.co.uk>
2025-10-23 19:21:58 +0100

    Fresh commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en" className={inter.variable}>
       <body className={cn('min-h-screen bg-background font-sans antialiased', inter.className)}>
         <NavBar />
-        {children}
+        {children} 
       </body>
     </html>
   )
